94b4249098bcd6befda34d8f76be138661803bfa
Dumenco <tt-3484@ukr.net>
2020-03-21 03:07:42 +0200

    color-switch

---
+++ js/index.js
@@ -0,0 +1,40 @@
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+  "#215553",
+  "#4CAF77",
+  "#AA5430",
+  "#045668",
+  "#123456"
+];
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const body = document.querySelector("body");
+const btnStart = document.querySelector('[data-action="start"]');
+const btnStop = document.querySelector('[data-action="stop"]');
+
+const onClick = {
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.Timer = setInterval(() => {
+      this.isActive = true;
+      body.style.backgroundColor = colors[randomIntegerFromInterval(0, 6)];
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.Timer);
+    this.isActive = false;
+  }
+};
+
+btnStart.addEventListener("click", onClick.start.bind(onClick));
+btnStop.addEventListener("click", onClick.stop.bind(onClick));
