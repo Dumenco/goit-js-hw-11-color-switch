a8722dba00ec6fb00b983a4b71beb1ffa795efee
Dumenco <tt-3484@ukr.net>
2020-03-25 22:33:36 +0200

    hw-11-color

--- js/index.js
+++ js/index.js
@@ -9,32 +9,48 @@ const colors = [
   "#4CAF77",
   "#AA5430",
   "#045668",
-  "#123456"
+  "#123456",
+  "#d78rjs",
+  "#22DRW3",
+  "#F00RTR",
+  "#2L7RI7",
+  "#102FOT",
+  "#RE23FD",
+  "#12D78F",
+  "#A1C7D0",
+  "#DPKBC1",
+  "#L4ER6S"
 ];
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+
 
 const body = document.querySelector("body");
 const btnStart = document.querySelector('[data-action="start"]');
 const btnStop = document.querySelector('[data-action="stop"]');
 
-const onClick = {
-  isActive: false,
-  start() {
-    if (this.isActive) {
-      return;
-    }
-    this.Timer = setInterval(() => {
-      this.isActive = true;
-      body.style.backgroundColor = colors[randomIntegerFromInterval(0, 6)];
+let isActive = false;
+let timer;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+  
+  function start() {
+  if (!isActive) {
+        timer = setInterval(() => {
+      body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+      isActive = true;
     }, 1000);
-  },
-  stop() {
-    clearInterval(this.Timer);
-    this.isActive = false;
-  }
+  };
+}
+
+function stop() {
+  clearInterval(timer);
+  isActive = false;
 };
 
-btnStart.addEventListener("click", onClick.start.bind(onClick));
-btnStop.addEventListener("click", onClick.stop.bind(onClick));
+
+btnStart.addEventListener("click", start);
+btnStop.addEventListener("click", stop);
+
+
+
